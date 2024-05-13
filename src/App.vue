833262d916aa4167a1920e07772592508d1bdfc1
melisasahira <melisasahira@student.uir.ac.id>
2024-05-13 17:30:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+import Todos from './components/Todos.vue';
+import Post from './components/Post.vue';
+const selectedMenu = ref('');
+
+const handleMenuClick = (menu) => {
+  selectedMenu.value = menu;
+};
+</script>
+
+<template>
+  <div>
+    <button @click="handleMenuClick('Todos')">Todos</button>
+    <button @click="handleMenuClick('Post')">Post</button>
+
+    <div>
+      <!-- Menampilkan komponen Todos jika menu Todos dipilih -->
+      <Todos v-if="selectedMenu === 'Todos'" />
+
+      <!-- Menampilkan komponen Post jika menu Post dipilih -->
+      <Post v-if="selectedMenu === 'Post'" />
+
+      <!-- Pesan ketika tidak ada menu yang dipilih -->
+      <p v-if="!selectedMenu">No menu selected</p>
+    </div>
+  </div>
+</template>
